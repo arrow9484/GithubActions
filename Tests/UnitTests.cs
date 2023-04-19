834b3fd456047caf3882fbebc9215e857beef469
arrow9484 <89360245+arrow9484@users.noreply.github.com>
2023-04-18 20:24:06 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -81,7 +81,7 @@ namespace GithubActionsLab
         [Test]
         public void Divide_Valid()
         {
-            Assert.AreEqual(1, Program.Divide("2", "1"));
+            Assert.AreEqual(2, Program.Divide("2", "1"));
             Assert.AreEqual(2, Program.Divide("4", "2"));
             Assert.AreEqual(5, Program.Divide("35", "7"));
         }
